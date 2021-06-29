ae47a739381a91d130a0f3f819bbbba1357d3496
Gage Atkinson <73555272+someStranger8@users.noreply.github.com>
2021-06-29 16:11:14 -0400

    Create _init_.py

---
+++ VA/_init_.py
@@ -0,0 +1,6 @@
+"""
+welcome to pogger virtual assistant.
+pogger is made to help you with your day.
+you can ask pogger any thing and he will respond.
+pogger also has built in commands such as clear, python3, shell, and exit.
+"""
